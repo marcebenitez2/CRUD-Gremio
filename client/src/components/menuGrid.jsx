853891abc066731862d77796bfb084bcd7f87d86
marcebenitez2 <marcebenitez0607@gmail.com>
2023-12-25 14:36:56 -0300

    Remove Bolson feature and related components

--- client/src/components/menuGrid.jsx
+++ client/src/components/menuGrid.jsx
@@ -5,18 +5,6 @@ function MenuGrid() {
   return (
     <div className="h-2/3 w-1/2 grid grid-cols-2 gap-x-2 gap-y-2 xln:w-full mdn:grid-cols-1 mdn:py-10 mdn:h-full">
       <Link
-        to="/menu/cajaNavidad"
-        className="flex items-center justify-center bg-red-700 rounded-lg text-2xl transition-all duration-300 hover:bg-red-800"
-      >
-        Caja navideña
-      </Link>
-      <Link
-        to="/menu/bolson"
-        className="flex items-center justify-center bg-green-700 rounded-lg text-2xl transition-all duration-300 hover:bg-green-800"
-      >
-        Bolson
-      </Link>
-      <Link
         to="/menu/empresas"
         className="flex items-center justify-center bg-blue-700 rounded-lg text-2xl transition-all duration-300 hover:bg-blue-800"
       >
